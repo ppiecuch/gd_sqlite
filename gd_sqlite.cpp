528d7ae0ec8a57001549a4347365f84e6f2015d1
Pawel Piecuch <pawel.piecuch@roche.com>
2021-04-02 20:33:47 +0200

    cleanup building issues and warnings

--- gd_sqlite.cpp
+++ gd_sqlite.cpp
@@ -4,10 +4,38 @@
 #include "core/project_settings.h"
 #include "core/os/os.h"
 
-SQLite::SQLite() {
+#include <string>
+
+Dictionary SQLite::deep_copy(Dictionary p_dict)
+{
+    Dictionary copy_dict;
+    Array keys = p_dict.keys();
+    int number_of_keys = keys.size();
+    for (int i = 0; i <= number_of_keys - 1; i++) {
+        copy_dict[keys[i]] = p_dict[keys[i]];
+    }
+    return copy_dict;
 }
 
-int SQLite::open(String path) {
+Variant SQLite::get_with_default(Dictionary p_dict, String p_key, Variant p_default)
+{
+    if (p_dict.has(p_key)) {
+        return p_dict[p_key];
+    }
+    else {
+        return p_default;
+    }
+}
+
+int SQLite::open(String p_path) {
+
+	String path = p_path;
+
+	String ending = String(".db");
+	if (!path.ends_with(ending)) {
+		path += ending;
+	}
+
 	if (path.begins_with("res://")) {
 		if (ProjectSettings::get_singleton()) {
 			String resource_path = ProjectSettings::get_singleton()->get_resource_path();
@@ -16,7 +44,7 @@ int SQLite::open(String path) {
 		}
 	}
 	else if (path.begins_with("user://")) {
-		String data_dir=OS::get_singleton()->get_data_path();
+		String data_dir = OS::get_singleton()->get_data_path();
 		if (data_dir != "") path = path.replace("user:/",data_dir);
 		else path = path.replace("user://", "");
 	}
@@ -24,15 +52,133 @@ int SQLite::open(String path) {
 	return sqlite3_open(path.utf8().get_data(), &db);
 }
 
-void SQLite::prepare(String query) {
-	sqlite3_prepare_v2(db, query.utf8().get_data(), -1, &stmt, NULL);
+void SQLite::prepare(String p_query) {
+
+	sqlite3_prepare_v2(db, p_query.utf8().get_data(), -1, &stmt, NULL);
+}
+
+
+bool SQLite::query_with_bindings(String p_query, Array param_bindings, Array query_result, bool verbose_mode)
+{
+    String err_msg;
+    int rc;
+
+    const char *sql = p_query.utf8().get_data();
+
+    /* Clear the previous query results */
+    query_result.clear();
+
+    sqlite3_stmt *stmt;
+    /* Prepare an SQL statement */
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    err_msg = String(sqlite3_errmsg(db));
+    if (rc != SQLITE_OK) {
+        ERR_PRINT("SQL error: " + err_msg)
+        return false;
+    }
+
+    /* Bind any given parameters to the prepared statement */
+    for (int i = 0; i < param_bindings.size(); i++) {
+        switch (param_bindings[i].get_type()) {
+            case Variant::NIL:
+				sqlite3_bind_null(stmt, i + 1);
+				break;
+
+			case Variant::BOOL:
+            case Variant::INT:
+                sqlite3_bind_int64(stmt, i + 1, int64_t(param_bindings[i]));
+                break;
+
+            case Variant::REAL:
+                sqlite3_bind_double(stmt, i + 1, param_bindings[i]);
+                break;
+
+            case Variant::STRING:
+                sqlite3_bind_text(stmt, i + 1, (param_bindings[i].operator String()).utf8().get_data(), -1, SQLITE_TRANSIENT);
+                break;
+
+            case Variant::POOL_BYTE_ARRAY: {
+                PoolByteArray binding = ((const PoolByteArray &)param_bindings[i]);
+                PoolByteArray::Read r = binding.read();
+                sqlite3_bind_blob64(stmt, i + 1, r.ptr(), binding.size(), SQLITE_TRANSIENT);
+                break;
+            }
+
+            default:
+                ERR_PRINT("GDSQLite Error: Binding a parameter of type " + String(std::to_string(param_bindings[i].get_type()).c_str()) + " (TYPE_*) is not supported!")
+                sqlite3_finalize(stmt);
+                return false;
+        }
+    }
+
+    // Execute the statement and iterate over all the resulting rows.
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+        Dictionary column_dict;
+        int argc = sqlite3_column_count(stmt);
+
+        /* Loop over all columns and add them to the Dictionary */
+        for (int i = 0; i < argc; i++)
+        {
+            Variant column_value;
+            /* Check the column type and do correct casting */
+            switch (sqlite3_column_type(stmt, i))
+            {
+            case SQLITE_INTEGER:
+                column_value = Variant((int64_t)sqlite3_column_int64(stmt, i));
+                break;
+
+            case SQLITE_FLOAT:
+                column_value = Variant(sqlite3_column_double(stmt, i));
+                break;
+
+            case SQLITE_TEXT:
+                column_value = Variant((char *)sqlite3_column_text(stmt, i));
+                break;
+
+            case SQLITE_BLOB: {
+                int bytes = sqlite3_column_bytes(stmt, i);
+                PoolByteArray arr = PoolByteArray();
+                arr.resize(bytes);
+                PoolByteArray::Write write = arr.write();
+                memcpy(write.ptr(), (char *)sqlite3_column_blob(stmt, i), bytes);
+                column_value = arr;
+                break;
+            }
+
+            case SQLITE_NULL:
+                break;
+
+            default:
+                break;
+            }
+
+            const char * col_name = sqlite3_column_name(stmt, i);
+            column_dict[String(col_name)] = column_value;
+        }
+        /* Add result to query_result Array */
+        query_result.append(column_dict);
+    }
+
+    // Clean up and delete the resources used by the prepared statement.
+    sqlite3_finalize(stmt);
+
+    rc = sqlite3_errcode(db);
+    err_msg = String(sqlite3_errmsg(db));
+    if (rc != SQLITE_OK) {
+        ERR_PRINT("SQL error: " + err_msg)
+        return false;
+    }
+
+    return true;
 }
 
 int SQLite::step() {
+
 	return sqlite3_step(stmt);
 }
 
 int SQLite::step_assoc() {
+
 	int ret = sqlite3_step(stmt);
 	if (_row_names.size() == 0) {
 		for(int i = 0; i < sqlite3_column_count(stmt); ++i)
@@ -42,6 +188,7 @@ int SQLite::step_assoc() {
 }
 
 Array SQLite::fetch_assoc() {
+
 	Array result;
 	while (1) {
 		int ret = sqlite3_step(stmt);
@@ -69,71 +216,86 @@ Array SQLite::fetch_assoc() {
 }
 
 Array SQLite::fetch_one() {
+
 	Array result = fetch_assoc();
 	if (result.size() <= 0) { return Array(); }
 	return result[0];
 }
 
-Array SQLite::fetch_array(String query) {
-	prepare(query);
+Array SQLite::fetch_array(String p_query) {
+
+	prepare(p_query);
 	Array result = fetch_assoc();
 	finalize();
 	return result;
 }
 
-int SQLite::query(String query) {
-	prepare(query);
+int SQLite::query(String p_query) {
+
+	prepare(p_query);
 	int ret = step();
 	finalize();
 	return ret;
 }
 
 int SQLite::get_data_count() {
+
 	return sqlite3_data_count(stmt);
 }
 
 int SQLite::get_column_count() {
+
 	return sqlite3_column_count(stmt);
 }
 
-int SQLite::get_column_int(int col) {
-	return sqlite3_column_int(stmt, col);
+int SQLite::get_column_int(int p_col) {
+
+	return sqlite3_column_int(stmt, p_col);
 }
 
-double SQLite::get_column_double(int col) {
-	return sqlite3_column_double(stmt, col);
+double SQLite::get_column_double(int p_col) {
+
+	return sqlite3_column_double(stmt, p_col);
 }
 
-String SQLite::get_column_text(int col) {
-	return String::utf8((char *)sqlite3_column_text(stmt, col));
+String SQLite::get_column_text(int p_col) {
+
+	return String::utf8((char *)sqlite3_column_text(stmt, p_col));
 }
 
-int SQLite::get_column_int_assoc(String col) {
-	return sqlite3_column_int(stmt, _row_names[col]);
+int SQLite::get_column_int_assoc(String p_col) {
+
+	return sqlite3_column_int(stmt, _row_names[p_col]);
 }
 
-double SQLite::get_column_double_assoc(String col) {
-	return sqlite3_column_double(stmt, _row_names[col]);
+double SQLite::get_column_double_assoc(String p_col) {
+
+	return sqlite3_column_double(stmt, _row_names[p_col]);
 }
 
-String SQLite::get_column_text_assoc(String col) {
-	return String::utf8((char *)sqlite3_column_text(stmt, _row_names[col]));
+String SQLite::get_column_text_assoc(String p_col) {
+
+	return String::utf8((char *)sqlite3_column_text(stmt, _row_names[p_col]));
 }
 
 void SQLite::finalize() {
+
 	sqlite3_finalize(stmt);
 	_row_names.clear();
 }
 
 String SQLite::get_errormsg() {
+
 	return sqlite3_errmsg(db);
 }
 
 void SQLite::close() {
+
 	sqlite3_close(db);
 }
 
 void SQLite::_bind_methods() {
+
 	ClassDB::bind_method("open", &SQLite::open);
 	ClassDB::bind_method("prepare", &SQLite::prepare);
 	ClassDB::bind_method("step", &SQLite::step);
@@ -189,3 +351,5 @@ void SQLite::_bind_methods() {
 	BIND_CONSTANT(SQLITE_DONE);
 }
 
+SQLite::SQLite() {
+}
--- gd_sqlite.h
+++ gd_sqlite.h
@@ -17,23 +17,24 @@ protected:
 public:
 	SQLite();
 	
-	int open(String path);
+	int open(String p_path);
 	
-	void prepare(String query);
+	void prepare(String p_query);
 	int step();
 	int step_assoc();
 	Array fetch_assoc();
 	Array fetch_one();
-	Array fetch_array(String query);
+	Array fetch_array(String p_query);
 	int query(String query);
+	bool query_with_bindings(String p_query, Array param_bindings, Array query_result, bool verbose_mode);
 	int get_data_count();
 	int get_column_count();
-	int get_column_int(int col);
-	double get_column_double(int col);
-	String get_column_text(int col);
-	int get_column_int_assoc(String col);
-	double get_column_double_assoc(String col);
-	String get_column_text_assoc(String col);
+	int get_column_int(int p_col);
+	double get_column_double(int p_col);
+	String get_column_text(int p_col);
+	int get_column_int_assoc(String p_col);
+	double get_column_double_assoc(String p_col);
+	String get_column_text_assoc(String p_col);
 	
 	void finalize();
 	String get_errormsg();
@@ -44,6 +45,9 @@ private:
 	sqlite3_stmt *stmt;
 
 	Map<String, unsigned int> _row_names;
+
+	Dictionary deep_copy(Dictionary p_dict);
+	Variant get_with_default(Dictionary p_dict, String p_key, Variant p_default);
 };
 
 #endif // GD_SQLITE_H
